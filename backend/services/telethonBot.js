09954d7236670efb991fa4924d1d5b1a0c0643f2
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-08-24 19:28:58 +0530

    dotenv implement

--- backend/services/telethonBot.js
+++ backend/services/telethonBot.js
@@ -2,15 +2,15 @@ const { TelegramClient } = require('telegram');
 const { StringSession } = require('telegram/sessions');
 const input = require('input');
 
-const apiId = 22126642;
+const apiId = parseInt(process.env.TELEGRAM_API_ID, 10);
 
-const apiHash = "8da73b7cb1bb436a56beaf556140e1c5";
+const apiHash = process.env.TELEGRAM_API_HASH;
 const stringSession = new StringSession(''); // fill this later with your saved session
 
 async function fetchChannelMessages(link) {
     const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
     await client.start({
-        phoneNumber: "+917001693312",
+        phoneNumber: process.env.PHONE_NUMBER,
         password: async () => await input.text('Password?'),
         phoneCode: async () => await input.text('Code?'),
         onError: (err) => console.log(err),
